996ec5f7ae156c90e2d259d6822c4c98d883a071
Stobles <stoble@mail.ru>
2026-09-22 22:31:58 +0300

    init: project initialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import * from '@/assets/hero.png'
+
+export default function App() {
+  
+
+  return (
+    <div className="flex h-screen w-screen overflow-hidden">
+      
+    </div>
+  );
+}
